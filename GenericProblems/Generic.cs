cc6760fca7ef58d69ed40d5ffd25bad43add469b
aamir0143 <mdaamirreza00@gmail.com>
2022-02-03 23:03:24 +0530

    [Aamir13] Add : Given 3 Strings Find The Maximum

--- GenericProblems/Generic.cs
+++ GenericProblems/Generic.cs
@@ -50,5 +50,26 @@ namespace GenericProblems
                 return 0.0;
             }
         }
+        //UC 3: Given 3 Strings Find The Maximum.
+        public static string FindMaxValueOutOfThree(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("\teither 2 or 3 values are equal");
+                return null;
+            }
+        }
     }
 }
--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -20,10 +20,13 @@ namespace GenericProblems
                 case 1:
                     int x = 30, y = 20, z = 10;
                     double p = 15.33, q = 45.77, r = 77.88;
+                    string s = "abc", s1 = "pqr", s2 = "xyz";
                     if (Generic.FindMaxValueOutOfThree(x, y, z) != 0)
                         Console.WriteLine("\t{0} is maximum out of {1},{2},{3}", Generic.FindMaxValueOutOfThree(x, y, z), x, y, z);
-                    if (Generic.FindMaxValueOutOfThree(p, q, r) != 0)
+                    if (Generic.FindMaxValueOutOfThree(p, q, r) != 0.0)
                         Console.WriteLine("\t{0} is maximum out of {1},{2},{3}", Generic.FindMaxValueOutOfThree(p, q, r), p, q, r);
+                    if (Generic.FindMaxValueOutOfThree(s, s1, s2) != null)
+                        Console.WriteLine("\t{0} is maximum out of {1},{2},{3}", Generic.FindMaxValueOutOfThree(s, s1, s2), s, s1, s2);
                     break;
                 case 2:
                 default:
